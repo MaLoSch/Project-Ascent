90522a72038f7da5592201ce45d2334d633fbc42
Markus Lorenz Schilling <hey@malosch.com>
2024-11-11 09:20:36 +0100

    Update to profile page
    Changed profile picture to something nondescript
    Made some CSS changes (mainly centring the content)
    Removed unused profile pictures from project

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,24 @@
-import profilePic from "../../assets/profile2.jpg"
+import profilePic from "../../assets/profile3.jpeg"
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import "./Profile.scss"
 
 function Profile() {
     return(
         <>
-        <section className="side-padding">
+        <section className="side-padding center">
             <img className="profile-pic" src={profilePic}></img>
         </section>
 
-        <section className="side-padding">
+        <section className="side-padding center">
             <p className="given-name">Profile handle</p>
             <p className="actual-name">Actual Name</p>
         </section>
 
-        <section className="side-padding">
+        <section className="side-padding center">
             <p className="description">Profile description goes here.</p>
         </section>
 
-        <section className="side-padding">
+        <section className="side-padding center">
             <p className="location">
             <LocationOnOutlinedIcon fontSize="12px" />
             <span className="city">City</span>, <span className="country">Country</span>
